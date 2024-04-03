4f8e01072287267594d766403866b56521f99956
Johanna Diaz <123778194+JohannaDiazR@users.noreply.github.com>
2024-04-02 21:17:54 -0500

    UsuarioModel

---
+++ Models/UsuarioModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Stel.Models
+{
+    public class UsuarioModel
+    {
+        public int id {  get; set; }
+        [Required(ErrorMessage = "El nombre de usuario es requerido")]
+        public string usuario {  get; set; }
+        [Required(ErrorMessage = "La contraseña es requerida")]
+        public string contrasena { get; set; }
+    }
+}
