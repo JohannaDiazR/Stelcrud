d0a77fbf44727d2f2d9e4810a82bc3b0f4312d86
Johanna Diaz <123778194+JohannaDiazR@users.noreply.github.com>
2024-04-02 21:17:15 -0500

    UsuarioDatos

---
+++ Data/UsuariosDatos.cs
@@ -0,0 +1,52 @@
+﻿using MySqlConnector;
+using Stel.Models;
+using System;
+using System.Data;
+using System.Linq;
+
+namespace Stel.Data
+{
+    public class UsuarioDatos
+    {
+        string connectionString = "Server=localhost;Database=stelnet;User Id=root;Allow Zero Datetime=True;";
+
+        public UsuarioModel ValidarCredenciales(string usuario, string contrasena)
+        {
+            UsuarioModel usuarioValidado = null;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM tbl_usuarios WHERE usuario = @usuario AND contrasena = @contrasena";
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@usuario", usuario);
+                        cmd.Parameters.AddWithValue("@contrasena", contrasena);
+
+                        using (MySqlDataReader dr = cmd.ExecuteReader())
+                        {
+                            if (dr.Read())
+                            {
+                                usuarioValidado = new UsuarioModel()
+                                {
+                                    id = Convert.ToInt32(dr["id"]),
+                                    usuario = dr["usuario"].ToString(),
+                                    contrasena = dr["contrasena"].ToString()
+                                };
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+
+            return usuarioValidado;
+        }
+    }
+}
+
